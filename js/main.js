7cb93f54399756ab7f643ff1d3687fbbbe992993
Ana Fernandez <anafdz92@gmail.com>
2020-06-30 10:31:33 +0200

    changes MAria

--- js/main.js
+++ js/main.js
@@ -6,13 +6,17 @@ const result = getRandomNumber(100);
 console.log(result);
 
 let boxNumber = document.querySelector(".js-box__text");
-let boxNumberValue = boxNumber.value;
-let boxNumberRealNumber = parseInt(boxNumberValue);
+//let boxNumberRealNumber = parseInt(boxNumberValue);
 const button = document.querySelector(".js-login-button");
 let message = document.querySelector(".js-message");
 let counter = document.querySelector(".js-counter");
+//let sum = 0 + 1;
 
 function check(ev) {
+  let inputValue = boxNumber.value;
+  console.log(inputValue);
+
+  /*
   if (boxNumberValue > 100 || boxNumberValue < 1) {
     message.innerHTML = "El número debe estar entre 1 y 100";
   } else {
@@ -23,8 +27,11 @@ function check(ev) {
     } else if (boxNumberValue === result) {
       message.innerHTML = "Has ganado Campeona!";
     }
-  }
-  counter.innerHTML = counter.innerHTML + parseInt(1);
+  }*/
+  counter = parseInt(counter.innerHTML);
+  console.log(counter);
+
+  //counter=` counter+parseInt(1)`
 }
 
 button.addEventListener("click", check);
